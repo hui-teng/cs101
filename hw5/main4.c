32581b043eecc9bb2f77a8f4aa204034f2b65f3c
hui-teng <w95chang@gmail.com>
2024-12-11 23:20:12 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,1 +1,8 @@
-
+int main()  {
+    int i = 12345;
+    int thousands = (i / 1000) % 10;
+    int units = i % 10;
+    i = i - thousands * 1000 - units + units * 1000 + thousands;
+    printf("結果: %d\n", i);
+    return 0;
+}
