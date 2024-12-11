6e6c9d76d4739c9dd003b98c551a5309c54feab3
hui-teng <w95chang@gmail.com>
2024-12-11 23:15:00 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,11 @@
-
+int main()  {
+    for (int index = 1; index <= 81; index++) {
+        int i = (index - 1) / 9 + 1;
+        int j = (index - 1) % 9 + 1;
+        printf("%d*%d=%d\t", i, j, i * j);
+        if (j == 9) {
+            printf("\n");
+        }
+    }
+    return 0;
+}
