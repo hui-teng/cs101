43111318ae4336ae98a85df723843fa0a5b91b96
hui-teng <w95chang@gmail.com>
2024-12-11 22:25:13 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,15 @@
-
+int main()  {
+    int n = 7;
+    for (int i = 1; i <= n; i++) {
+        int total = n + i - 1;
+        for (int j = 1; j <= total; j++) {
+            if (j <= n - i) {
+                printf("  ");
+            } else {
+                printf("%d ", i);
+            }    
+        }
+        printf("\n");
+}
+    return 0;
+}
